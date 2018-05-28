169243fa9bf7ebd57b52b204e05a54234c6cb468
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 19:02:11 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,7 @@ class Actor < ActiveRecord::Base
      return "#{self.first_name} #{self.last_name}"
   end
 
-  def list_shows
+  def list_roles
     binding.pry
     self.shows.map do |show|
       puts show
