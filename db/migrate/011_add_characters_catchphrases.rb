dc36dbe858b7e788324167d2d2467c6588a067a6
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 18:38:11 +0000

    automatically backed up by learn

--- db/migrate/011_add_characters_catchphrases.rb
+++ db/migrate/011_add_characters_catchphrases.rb
@@ -1,4 +1,4 @@
-class AddToCharactersCatchphrases < ActiveRecord::Migration[4.2]
+class AddCharactersCatchphrases < ActiveRecord::Migration[4.2]
 
   def change
     add_column :characters, :catchphrase, :string
